e09cbda1cba9517d77cf001e4745f60840e839b4
Wiss-a <125835967+Wiss-a@users.noreply.github.com>
2025-01-14 14:34:25 +0000

    Delete travel-chatbot/Components directory

--- travel-chatbot/Components/SplineScene3.jsx
+++
@@ -1,8 +0,0 @@
-import Spline3 from '@splinetool/react-spline';
-
-
-export default function SplineScene3() {
-  return (
-    <Spline3 scene="https://prod.spline.design/7Zrnt-zC1XfFkzSx/scene.splinecode" />
-);
-}
